b837fcd1d3882e0e530c352c16fb58cd3da76b29
Priit Lätt <lattpriit@gmail.com>
2024-01-04 12:59:52 +0200

    Initial Commit

---
+++ ClickReceiver/ClickReceiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickReceiverApp.swift
+//  ClickReceiver
+//
+//  Created by Priit Lätt on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClickReceiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
